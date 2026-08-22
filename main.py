2813cd6bd0893bdae1956098906f73a031375cb6
SANTOOS00 <mohamederraiss@icloud.com>
2026-08-22 22:02:10 +0100

    .

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from llm_sdk.llm_sdk import Small_LLM_Model
 
 
-def test_method() -> None:
-    ss = Small_LLM_Model()
+class Main:
+    @staticmethod
+    def run()
+    pass
+    
+    # ss = Small_LLM_Model()
 
 
 if __name__ == "__main__":
-    # print("test")
-    test_method()
+    try:
+        Main().run()
+    except 
+# uv run python -m src
+# --functions_definition data/input/functions_definition.json
+# --input data/input/function_calling_tests.json
+# --output data/output/function_calls.json
