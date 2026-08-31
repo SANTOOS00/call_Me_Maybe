70d0e9c2e6f59591450ea59f60a0679628491b79
SANTOOS00 <mohamederraiss@icloud.com>
2026-08-31 11:39:34 +0100

    .

--- src/__main__.py
+++ src/__main__.py
@@ -1,20 +1,27 @@
 from .parser import Parser
 from .custom_error import Call_Error
+from llm_sdk.llm_sdk import Small_LLM_Model
 
 
 class Main:
+    data: Parser
+
     @classmethod
     def run(cls) -> None:
         cls.parser()
+        cls.run_module()
 
-    @staticmethod
-    def parser() -> None:
-        parser = Parser()
-        parser.run()
+    @classmethod
+    def parser(cls) -> None:
+        cls.data = Parser()
+        cls.data.run()
 
-    @staticmethod
-    def run_module() -> None:
-        pass
+    @classmethod
+    def run_module(cls) -> None:
+        module = Small_LLM_Model()
+        prompt = cls.data.get_prompts()[0]
+        ss = module.encode(prompt.prompt)
+        print(ss)
 
 
 if __name__ == "__main__":
